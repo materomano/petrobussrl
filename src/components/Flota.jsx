c6d1cdc9e9a9bc3208083f092dca728e6404f26b
mateo romano <mate.romano83@gmail.com>
2025-01-11 01:53:54 -0300

    petrobus

--- src/components/Flota.jsx
+++ src/components/Flota.jsx
@@ -102,7 +102,7 @@ const Flota = () => {
               className={styles.modalImage}
             />
             <button onClick={handleNextImage} className={`${styles.carouselButton} ${styles.right}`}>
-              <img src="/images/logo.jpg" alt="" />
+              <img src="/images/flechaderecha.jpg" alt="" />
             </button>
           </div>
         )}
